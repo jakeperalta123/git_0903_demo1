ac771cf7df2390a6617f7f7e45fb571894d2bb4b
jake <iloveheat1105@gmail.com>
2022-09-03 14:12:59 +0800

    initial commit

---
+++ src/main/java/com/example/demo/Git0903DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git0903DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git0903DemoApplication.class, args);
+	}
+
+}
